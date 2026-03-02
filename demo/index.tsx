6db30a4778155778b6afbd5c0ff6a94f450c2a2f
Michael Milton <michael.r.milton@gmail.com>
2026-03-02 12:12:45 +1100

    Remove demo css

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
